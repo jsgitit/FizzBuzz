e307e829d42e7ef14418d8edbd8379ade88ccadc
Jon Smart <jsmartware@gmail.com>
2020-07-11 18:06:28 -0700

    Added code for FizzBuzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = "";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result = "FizzBuzz";
+                    Console.WriteLine(result);
+                }
+                else if (i % 3 == 0)
+                {
+                    result = "Fizz";
+                    Console.WriteLine(result);
+                }
+                else if (i % 5 == 0)
+                {
+                    result = "Buzz";
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    result = i.ToString();
+                    Console.WriteLine(result);
+                }
+            }
         }
     }
 }
