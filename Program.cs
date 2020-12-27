5d3b1a213d349a887ac24ffe03f160e5582bdd76
Jon <jsmartware@aol.com>
2020-12-26 21:04:49 -0800

    refactored to make more concise.

--- Program.cs
+++ Program.cs
@@ -10,25 +10,14 @@ namespace FizzBuzz
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 15 == 0)
-                {
                     result = "FizzBuzz";
-                    Console.WriteLine(result);
-                }
                 else if (i % 3 == 0)
-                {
                     result = "Fizz";
-                    Console.WriteLine(result);
-                }
                 else if (i % 5 == 0)
-                {
                     result = "Buzz";
-                    Console.WriteLine(result);
-                }
                 else
-                {
                     result = i.ToString();
-                    Console.WriteLine(result);
-                }
+                Console.WriteLine(result);
             }
         }
     }
